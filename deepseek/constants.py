032b51de7699b1864d1eec2e446437a71ebe9751
diego-joubert <diegojouberttellez@gmail.com>
2026-04-14 10:08:31 -0400

    feat(deepseek-gtk): Definir constantes de la aplicacion

---
+++ deepseek/constants.py
@@ -0,0 +1,10 @@
+"""Constantes para el cliente DeepSeek GTK"""
+
+APP_NAME = "deepseek-gtk"
+WINDOW_TITLE = "DeepSeek Chat"
+DEFAULT_WIDTH = "1200"
+DEFAULT_HEIGHT = "800"
+DEEPSEEK_URL = "https://chat.deepseek.com"
+DEFAULT_USER_AGENT = (
+    "Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0"
+)
