ef2a2b3001840f6dc1823aa0a2277cd0cf7c32ac
diego-joubert <diegojouberttellez@gmail.com>
2026-04-14 13:43:48 -0400

    feat(deepseek-gtk): Implementar __init__.py con informacion sobre la aplicacion

---
+++ deepseek/__init__.py
@@ -0,0 +1,10 @@
+"""
+DeepSeek GTK
+-------------------
+
+Un cliente no oficial y ligero para DeepSeek Chat escrito en Python utilizando WebKit y GTK3.
+
+Autor: Diego Joubert
+Version: 0.1
+Licencia: GPLv3
+"""
